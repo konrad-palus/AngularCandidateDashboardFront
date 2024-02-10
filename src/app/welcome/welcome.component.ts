046ebccd71e7a94d6e2c331702c382fccd4f03a5
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-02-10 18:10:21 +0100

    registration  UI done

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { SignInComponent } from '../../app/sign-in/sign-in.component'; 
+import { RegisterComponent } from '../../app/register/register.component';
+import { ForgotPasswordComponent } from '../../app/forgot-password/forgot-password.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [SignInComponent, RegisterComponent, ForgotPasswordComponent, CommonModule],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.css'
+  styleUrls: ['./welcome.component.css'] 
 })
 export class WelcomeComponent {
+  currentForm: 'sign' | 'register' | 'forgot' | null = null;
 
+  showForm(form: 'sign' | 'register' | 'forgot' | null) {
+    this.currentForm = form;
+  }
 }
